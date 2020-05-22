7d2ecea2d3cd726e16ccb61bca463fed00b2f777
Kevin Holder <kholder777@yahoo.com>
2020-05-22 00:02:09 -0500

    All bugs removed. Not MVP yet.

--- Assets/index.js
+++ Assets/index.js
@@ -1,11 +1,16 @@
 // Api key
 var apiKey = "4f860fe079fa9a65d76c6faf6564181c";
+
+// var apiKey = "166a433c57516f51dfab1f7edaed8413";
+
+// Here we are building the URL we need to query the database
+
 // Empty array for cities
 var cities = [];
 // Temp empty vars POSSIBLY not a good idea to have global. might need to be local vars.
 var lonCurrentCity = "";
 var latCurrentCity = "";
-var city = "";
+var city = "Austin";
 // UV site: https://openweathermap.org/api/uvi
 var uvCurrentCityQueryURL =
 	"http://api.openweathermap.org/data/2.5/uvi?appid=" +
@@ -28,21 +33,27 @@ var fiveDayCurrentCityQueryURL =
 	apiKey;
 
 // displaycityInfo function re-renders the HTML to display the appropriate content
-function displaycityInfo() {
+function displayCityInfo(queryURL, type) {
 	var city = $(this).attr("#city-name");
 	// var city = $(this).attr("data-name");
 
-	// -----QUESTION-----can i get rid of this and just put the variable "weatherCurrentCityQueryURL" in its place?
-	var queryURL =
-		"https://api.openweathermap.org/data/2.5/forecast?q=" +
-		city +
-		"&appid=" +
-		apiKey;
 	console.log(queryURL);
 
 	// Creating an AJAX call for the specific city button being clicked
 	$.ajax({
 		url: queryURL,
 		method: "GET",
-	}).then(function (response) {});
+	}).then(function (response) {
+		//
+		// response stuff here. prob an if/else statement
+		console.log(response);
+	});
 }
+// per the function, uses a (keyKEY, value) pair. the value can be a single thing, or an array, ("tho i dont know why you'd like to make it an array." -Antonio)
+displayCityInfo(weatherCurrentCityQueryURL, ["windSpeed"]);
+displayCityInfo(fiveDayCurrentCityQueryURL);
+// displayCityInfo(
+// 	"https://api.openweathermap.org/data/2.5/weather?" +
+// 		"q=Bujumbura,Burundi&appid=" +
+// 		apiKey
+// );
